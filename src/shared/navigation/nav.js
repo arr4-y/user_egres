e8a462f36dd6beeffc486823898b9305e6f0e0a6
Ar4x365 <80538700+arr4-y@users.noreply.github.com>
2024-11-06 16:03:18 +0000

    110524

--- src/shared/navigation/nav.js
+++ src/shared/navigation/nav.js
@@ -8,10 +8,10 @@ export default function Header() {
 
     const toggleMenu = () => setIsOpen(!isOpen);
 
-    // Effect to close the menu when resizing to a large screen
+    // Cerrar el menú cuando se redimensiona la pantalla a un tamaño mayor
     useEffect(() => {
         const handleResize = () => {
-            if (window.innerWidth >= 768) { // Assuming 'md' starts at 768px
+            if (window.innerWidth >= 768) {
                 setIsOpen(false);
             }
         };
@@ -20,54 +20,41 @@ export default function Header() {
         return () => window.removeEventListener("resize", handleResize);
     }, []);
 
-    return (
-        <header className="flex justify-between items-center px-4 py-4 bg-white shadow-md md:px-8">
-            <Link href="/" className="text-[#FE6A00] text-2xl font-bold">
-                UNFV
-            </Link>
+    // Cerrar el menú cuando se hace clic en un enlace en móviles
+    const closeMenu = () => {
+        if (isOpen) {
+            setIsOpen(false);
+        }
+    };
 
-            <button
-                className="text-[#FE6A00] md:hidden focus:outline-none"
-                onClick={toggleMenu}
-            >
-                <svg
-                    className="w-6 h-6"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M4 6h16M4 12h16M4 18h16"
-                    ></path>
-                </svg>
-            </button>
+    return (
+        <header className="flex justify-between items-center p-6 bg-white shadow-md w-full">
+            <div className="flex items-center gap-4">
+                <Link href="/" className="text-[#FE6A00] text-2xl font-bold">
+                    UNFV
+                </Link>
+            </div>
 
             <nav
-                className={`${isOpen ? "flex flex-col gap-2 items-center" : "hidden"} 
-                            md:flex md:items-center space-y-4 md:space-y-0 md:space-x-6 
-                            absolute md:relative top-16 left-0 w-full bg-white shadow-md 
-                            md:shadow-none md:top-auto md:w-auto p-4 md:p-0 z-10`}
+                id="primary-navigation"
+                className={`flex items-center space-x-6 md:flex transition-all duration-300 ease-in-out gap-4`}
             >
-                <Link href="/nosotros" className="text-gris-o1 hover:text-main transition duration-300">
+                <Link href="/nosotros" className="text-gray-800 hover:text-[#FE6A00] transition duration-300" onClick={closeMenu}>
                     Sobre nosotros
                 </Link>
-                <Link href="/noticias" className="text-gris-o1 hover:text-main transition duration-300">
+                <Link href="/noticias" className="text-gray-800 hover:text-[#FE6A00] transition duration-300" onClick={closeMenu}>
                     Noticias
                 </Link>
-                <Link href="/convenios" className="text-gris-o1 hover:text-main transition duration-300">
+                <Link href="/convenios" className="text-gray-800 hover:text-[#FE6A00] transition duration-300" onClick={closeMenu}>
                     Convenios
                 </Link>
-                <Link href="/egresados" className="text-gris-o1 hover:text-main transition duration-300">
+                <Link href="/egresados" className="text-gray-800 hover:text-[#FE6A00] transition duration-300" onClick={closeMenu}>
                     Nuestros egresados
                 </Link>
-                <Link href="/bolsaTrabajo" className="text-gris-o1 hover:text-main transition duration-300">
+                <Link href="/bolsaTrabajo" className="text-gray-800 hover:text-[#FE6A00] transition duration-300" onClick={closeMenu}>
                     Bolsa de trabajo
                 </Link>
-                <Link href="/login" className="block bg-[#FE6A00] text-white px-4 py-2 rounded-md shadow-sm text-center hover:bg-orange-500 transition duration-300">
+                <Link href="/login" className="bg-[#FE6A00] text-white px-4 py-2 rounded-full shadow-sm text-center hover:bg-orange-500 transition duration-300" onClick={closeMenu}>
                     Iniciar sesión
                 </Link>
             </nav>
